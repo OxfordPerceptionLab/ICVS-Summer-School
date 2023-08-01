89175f8ca9c192673d584ac38ce06efcafc71710
Dana Turner <dana.turner@psy.ox.ac.uk>
2023-08-01 10:48:33 +0100

    code  cleanup

---
+++ Arduino-Leonardo/CAM-Code/SetConstants.m
@@ -0,0 +1,16 @@
+function constants = SetConstants
+
+constants = struct;
+
+% Set constants
+constants.redAnchor = 50;
+constants.greenAnchor = 350;
+
+constants.referenceColourSeconds = 2;
+constants.adaptationSeconds = 3;
+
+constants.yellowReferenceBrightness = 160;
+constants.redAdaptationBrightness = 255;
+constants.greenAdaptationBrightness = 255;
+
+end
