e7036d9b6bab22156cb48e009d2f592ab9ba2987
Dana Turner <dana.turner@psy.ox.ac.uk>
2023-08-07 16:24:56 +0100

    dNum

--- Arduino-Leonardo/RLM-Code/writeLEDs.m
+++ Arduino-Leonardo/RLM-Code/writeLEDs.m
@@ -3,7 +3,7 @@ function writeLEDs(a, RGY)
 % Locations of red, green, and yellow LED
 dNum = ["D6", "D5", "D9"];      % Blue = "D3"
 
-for i = 1:3
+for i = 1:length(dNum)
     colVal = (255 - RGY(i)) / 255;
     writePWMDutyCycle(a, dNum(i), colVal);
 end
